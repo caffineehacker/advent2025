81231f78d3fe0b58bcdf46da30cf63e14f44740c
Tim Waterhouse <tim@timwaterhouse.com>
2025-12-01 21:41:01 -0800

    Part 1

---
+++ day02/src/main.rs
@@ -0,0 +1,186 @@
+use clap::Parser;
+use itertools::Itertools;
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(long, default_value = "")]
+    data_file: String,
+    #[arg(long)]
+    debug: bool,
+}
+
+// #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+// struct Position {
+//     x: i64,
+//     y: i64,
+// }
+
+#[derive(Debug, Clone, Hash)]
+struct Input {
+    values: Vec<(i64, i64)>,
+}
+
+fn main() {
+    let args = Args::parse();
+    let data_file = if args.data_file.is_empty() {
+        format!("{}/src/data.txt", env!("CARGO_MANIFEST_DIR"))
+    } else {
+        args.data_file
+    };
+
+    let input = parse(&data_file);
+
+    let result1 = part1(&input);
+    println!("Part1: {}", result1);
+
+    println!("Part 2: {}", part2(&input))
+}
+
+fn part1(input: &Input) -> i64 {
+    // NOTES:
+    // Odd number of digits is always valid and can be skipped
+    // For each number of digits you can just increment half the digits to find the duplicates
+    let mut sum = 0;
+    for (start, end) in input.values.iter() {
+        let start_str = start.to_string();
+        let mut current_length = start_str.len();
+
+        let end_str = end.to_string();
+        let end_length = end_str.len();
+
+        if current_length % 2 == 1 {
+            current_length += 1;
+        }
+
+        while current_length <= end_length {
+            // Numbers can't start with 0
+            let mut current_number = 10_i64
+                .pow(((current_length as u32) / 2) - 1)
+                .max(*start / 10_i64.pow((current_length as u32) / 2) - 1);
+            let end_number = (*end).min("9".repeat(end_length).parse::<i64>().unwrap());
+
+            while current_number <= end_number {
+                let current_str = current_number.to_string() + &current_number.to_string();
+                let new_num = current_str.parse::<i64>().unwrap();
+                if new_num > end_number {
+                    break;
+                }
+                if new_num >= *start {
+                    sum += new_num;
+                }
+                current_number += 1;
+            }
+            current_length += 2;
+        }
+    }
+
+    return sum;
+}
+
+fn part2(input: &Input) -> i64 {
+    0
+}
+
+fn parse(file: &str) -> Input {
+    let file = File::open(file).expect("Failed to open file");
+    let reader = BufReader::new(file);
+    let lines: Vec<String> = reader
+        .lines()
+        .map(|line| line.expect("Failed to read line"))
+        .collect();
+
+    Input {
+        values: lines
+            .iter()
+            .flat_map(|line| {
+                line.split(',').map(|v| {
+                    let (start, end) = v.split_once('-').unwrap();
+                    (start.parse::<i64>().unwrap(), end.parse::<i64>().unwrap())
+                })
+            })
+            .collect_vec(),
+    }
+
+    /*
+     * Alternative implementations:
+     */
+
+    // Two sections separated by a newline
+    // Input {
+    //     first: lines
+    //         .iter()
+    //         .take_while(|line| !line.is_empty())
+    //         .map(|line| line.split_once('|').unwrap())
+    //         .map(|(a, b)| (a.parse::<i64>().unwrap(), b.parse::<i64>().unwrap()))
+    //         .collect_vec(),
+    //     second: lines
+    //         .iter()
+    //         .skip_while(|line| !line.is_empty())
+    //         .filter(|line| !line.is_empty())
+    //         .map(|line| {
+    //             line.split(',')
+    //                 .map(|page| page.parse::<i64>().unwrap())
+    //                 .collect_vec()
+    //         })
+    //         .collect_vec(),
+    // }
+
+    // Creates a HashMap<char, Vec<Position>>
+    // let map_limits = Position {
+    //     x: lines[0].len() as i64,
+    //     y: lines.len() as i64,
+    // };
+
+    // Input {
+    //     antennas: lines
+    //         .into_iter()
+    //         .enumerate()
+    //         .flat_map(|(y, line)| {
+    //             line.chars()
+    //                 .enumerate()
+    //                 .filter(|(_, c)| *c != '.')
+    //                 .map(|(x, c)| {
+    //                     (
+    //                         c,
+    //                         Position {
+    //                             x: x as i64,
+    //                             y: y as i64,
+    //                         },
+    //                     )
+    //                 })
+    //                 .collect_vec()
+    //         })
+    //         .sorted_by(|(a, _), (b, _)| Ord::cmp(a, b))
+    //         .chunk_by(|(c, _)| *c)
+    //         .into_iter()
+    //         .map(|(c, positions)| (c, positions.map(|(_, p)| p).collect_vec()))
+    //         .collect(),
+    //     map_limits,
+    // }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let input = parse(&(env!("CARGO_MANIFEST_DIR").to_owned() + "/src/test1.txt"));
+        let result1 = part1(&input);
+
+        assert_eq!(result1, 1227775554);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = parse(&(env!("CARGO_MANIFEST_DIR").to_owned() + "/src/test1.txt"));
+        let result2 = part2(&input);
+
+        assert_eq!(result2, 0);
+    }
+}
